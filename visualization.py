e5ae4a9ca9b017b7f9f75ec0d7f2cb0689285148
sabuna-gamal <sabunagko@gmail.com>
2025-05-20 13:13:56 +0545

    updated to fix error

--- visualization.py
+++ visualization.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
-# ----------------------- Updated Plotting Functions -----------------------
 
 def plot_temperature_trends(df):
     """
